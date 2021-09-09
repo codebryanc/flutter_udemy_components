1164744fbf3c387bcf5d2f18833589bc42240562
Bryan Alejandro Cubillos Prieto <bryan.cubillos@bizagi.com>
2021-09-08 23:17:36 -0500

    Dates

--- lib/src/pages/input_page.dart
+++ lib/src/pages/input_page.dart
@@ -7,6 +7,11 @@ class InputPage extends StatefulWidget {
 
 class _InputPageState extends State<InputPage> {
   String _nombre = '';
+  String _email = '';
+  String _password = '';
+  String _fecha = '';
+
+  TextEditingController _inputDateFieldController = new TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -18,6 +23,12 @@ class _InputPageState extends State<InputPage> {
           padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
           children: <Widget>[
             _crearInput(),
+            Divider(),
+            _crearEmail(),
+            Divider(),
+            _crearPassword(),
+            Divider(),
+            _crearFecha(context),
             Divider(),
             _crearPersona(),
           ]),
@@ -47,7 +58,85 @@ class _InputPageState extends State<InputPage> {
 
   Widget _crearPersona() {
     return ListTile(
-      title: Text('La persona creada es: $_nombre'),
+      title: Text('Nombre: $_nombre'),
+      subtitle: Text('Email: $_email'),
     );
   }
+
+  Widget _crearEmail() {
+    return TextField(
+        // autofocus: true,
+        // textCapitalization: TextCapitalization.words,
+        keyboardType: TextInputType.emailAddress,
+        decoration: InputDecoration(
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+          // counter: Text('Letras ${_nombre.length}'),
+          hintText: 'Email',
+          labelText: 'Email',
+          // helperText: 'Sólo es el nombre',
+          suffixIcon: Icon(Icons.alternate_email),
+          icon: Icon(Icons.email),
+        ),
+        onChanged: (valor) => setState(() {
+              _email = valor;
+            }));
+  }
+
+  Widget _crearPassword() {
+    return TextField(
+        // autofocus: true,
+        // textCapitalization: TextCapitalization.words,
+        // keyboardType: TextInputType.visiblePassword,
+        obscureText: true,
+        decoration: InputDecoration(
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+          // counter: Text('Letras ${_nombre.length}'),
+          hintText: 'Password',
+          labelText: 'Password',
+          // helperText: 'Sólo es el nombre',
+          suffixIcon: Icon(Icons.lock_open),
+          icon: Icon(Icons.lock),
+        ),
+        onChanged: (valor) => setState(() {
+              _password = valor;
+            }));
+  }
+
+  Widget _crearFecha(BuildContext context) {
+    return TextField(
+      enableInteractiveSelection: false,
+      controller: _inputDateFieldController,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+        // counter: Text('Letras ${_nombre.length}'),
+        hintText: 'Fecha de nacimiento',
+        labelText: 'Fecha de nacimiento',
+        // helperText: 'Sólo es el nombre',
+        suffixIcon: Icon(Icons.perm_contact_cal),
+        icon: Icon(Icons.date_range),
+      ),
+      onTap: () {
+        FocusScope.of(context).requestFocus(new FocusNode());
+        _selectDate(context);
+      },
+    );
+  }
+
+  _selectDate(BuildContext context) async {
+    DateTime picked = await showDatePicker(
+      context: context,
+      initialDate: new DateTime.now(),
+      firstDate: new DateTime(2018),
+      lastDate: new DateTime(2025)
+    );
+
+    if(picked != null) {
+      setState(() {
+        _fecha = picked.toString();
+        _inputDateFieldController.text = _fecha;
+      });
+    }
+  }
+
+
 }
