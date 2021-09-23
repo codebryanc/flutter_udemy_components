d70d4787e14a48c276e0f749f46203fe40e89983
Bryan Alejandro Cubillos Prieto <bryan.cubillos@bizagi.com>
2021-09-23 14:02:22 -0500

    Scroll

--- lib/src/pages/listview_page.dart
+++ lib/src/pages/listview_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 class ListaPage extends StatefulWidget {
   @override
@@ -7,20 +8,51 @@ class ListaPage extends StatefulWidget {
 
 class _ListaPageState extends State<ListaPage> {
 
-  List<int> _numbers = [1,32,35,94,75,26];
+  ScrollController _scrollController = new ScrollController();
+  List<int> _numbers = [];
+  int _lastNumber = 0;
+  bool _isLoading = false;
   
+  @override
+  void initState() {
+    // Clase state y la inicializa! 
+    super.initState();
+
+    _agregarDiez();
+
+    _scrollController.addListener(() {
+      if(_scrollController.position.pixels == _scrollController.position.maxScrollExtent) {
+        // Llegue al final de la pagina
+        _fetchData();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    // Para prevenir fugas de memoria
+    _scrollController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Listas'),
       ),
-      body: _crearLista(),
+      body: Stack(
+        children: <Widget>[
+          _crearLista(),
+          _crearLoading()
+        ],
+      )
     );
   }
 
   Widget _crearLista() {
     return ListView.builder(
+      controller: _scrollController,
       itemCount: _numbers.length,
       itemBuilder: (BuildContext context, int index) {
         
@@ -32,5 +64,66 @@ class _ListaPageState extends State<ListaPage> {
         );
       },
     );
+  }
+
+  void _agregarDiez() {
+    for (var i = 0; i < 10; i++) {
+      // Aumento ++
+      _lastNumber++;
+      _numbers.add(_lastNumber);
+    }
+
+    setState(() {
+      
+    });
+  }
+
+  Future<Null> _fetchData() async {
+    // Le digo a Flutter que se debe redibujar por qué esa propiedad cambio
+    setState(() {
+      _isLoading = true;
+    });
+
+    final duration = new Duration(seconds: 2); // => Pasan estos 2 segundos pero el usuario no sabe que sucede
+
+    return new Timer(duration, _respuestaHTTP);
+  }
+
+  void _respuestaHTTP() {
+    
+    _isLoading = false;
+
+    _scrollController.animateTo(
+      _scrollController.position.pixels + 100,
+      curve: Curves.fastOutSlowIn,
+      duration: Duration(milliseconds: 200)
+    );
+
+    _agregarDiez();
+  }
+
+  // Siempre debemos devolver algo
+  Widget _crearLoading() {
+    if(_isLoading) {
+      return Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircularProgressIndicator(
+                color: Colors.white,
+              )
+            ]
+          ),
+          SizedBox(height: 15.0),
+        ]
+      );
+    }
+    else {
+      // Devolvemos un contenedor vacío
+      return Container();
+    }
   }
 }
